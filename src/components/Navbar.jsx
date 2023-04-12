82f444fad2bf3e596b8d894a637db0c989b4d56f
Jocsan Morera <joey_morera13@hotmail.com>
2023-04-11 19:10:19 -0600

    update logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Nav, NavLink, NavIcon } from './NavbarElements';
+import logo from '../../src/assets/page/urban.png'
 
 const Navbar = ({ toggle }) => {
   return (
     <>
       <Nav>
-        <NavLink to='/'><img src="../../src/assets/urban.png" alt="" 
+        <NavLink to='/'><img src={logo} alt="logo de la empresa" 
                   className='w-48 mt-4 ml-[76px]  lg:w-60 lg:pt-60' /></NavLink>
         <NavIcon onClick={toggle} className='top-72 right-32 text-4xl 
                                             lg:top-20 lg:right-20 lg:text-5xl'>
